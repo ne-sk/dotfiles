7b9999dcb2a8d4645eaa72a84f9bb82c4acf7c9c
Elvis Nipah <elvisnipah@gmail.com>
2026-01-05 09:21:10 +0000

    additions

---
+++ dot_config/nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,13 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_background = "medium"
+    -- vim.cmd.colorscheme("gruvbox-material")
+  end,
+}
