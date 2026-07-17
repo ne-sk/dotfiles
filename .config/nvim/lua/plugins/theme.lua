baa3c303f501e79986f1e160cd5d2313cbd5f4a7
Elvis Nipah <elvisnipah@gmail.com>
2026-07-17 13:31:04 +0000

    nvim: choose gruvbox & new colorizer plugin

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,4 @@
--- vim.pack.add({ "https://github.com/sainnhe/gruvbox-material" })
--- vim.g.gruvbox_material_background = "hard"
--- vim.g.gruvbox_material_better_performance = 1
--- vim.cmd("colorscheme gruvbox-material")
-
-vim.pack.add({ "https://github.com/shaunsingh/nord.nvim" })
-vim.cmd("colorscheme nord")
-vim.g.nord_borders = true
-vim.g.contrast = true
--- vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
--- vim.cmd("colorscheme tokyonight-night")
-
--- vim.pack.add({ "https://github.com/sainnhe/everforest" })
--- vim.g.everforest_enable_italic = true
--- vim.g.everforest_background = "hard"
--- vim.cmd("colorscheme everforest")
-
--- vim.pack.add({ "https://github.com/sainnhe/edge" })
--- vim.g.edge_better_performance = 1
--- vim.g.edge_style = "aura"
--- vim.cmd("colorscheme edge")
+vim.pack.add({ "https://github.com/sainnhe/gruvbox-material" })
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_better_performance = 1
+vim.cmd("colorscheme gruvbox-material")
